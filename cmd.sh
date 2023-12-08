0d25eab8b5d666e92cf5587e01b4f05caa343a17
arunkumar <arunkumar.subramaniyan@ecobillz.com>
2023-12-08 18:07:02 +0530

    cmd update

--- cmd.sh
+++ cmd.sh
@@ -5,3 +5,6 @@ docker image ls
 docker image rm image_id
 
 sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' 530143e7e52e
+
+
+docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
